d85ccd48ca341e5c4af70b8327b4046101ec8781
Eva <as45986@gmail.com>
2023-04-08 20:58:02 +0800

    fix:searchBar modify focus color

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
-const props = defineProps({
-  inputValue: String,
-});
+export interface Props {
+  inputValue?: string,
+  focusColor?: string,
+  defaultIcon: boolean,
+  placeholder: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  defaultIcon: false,
+  placeholder: 'Enter what you what to search...'
+})
+
 const emit = defineEmits(["update:input-value","submitValue"]);
 const updateValue = (event:any) => {
   emit("update:input-value", event.target.value);
@@ -13,10 +22,14 @@ const submitEvent = ()=>{
 <template>
   <form>   
       <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-      <div class="relative">
+        <div class="relative">
+            <div v-if="defaultIcon" class="absolute inset-y-0 left-0 flex z-10 items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round"      stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+          </div>
           <slot name="icon"></slot>
-          <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required :value="inputValue" @input="updateValue" >
+          <input type="search" id="default-search" :class="['block', 'w-full', 'p-4', 'pl-10', 'text-sm', 'text-gray-900' ,'border', 'border-gray-300', 'rounded-lg', 'bg-gray-50',  'dark:bg-gray-700', 'dark:border-gray-600', 'dark:placeholder-gray-400', 'dark:text-white','focus:outline-none',, 'focus:ring-2',[focusColor ? focusColor : 'focus:ring-blue-600'] ]" :placeholder=placeholder required :value="inputValue" @input="updateValue" >
           <slot name="button" @click="submitEvent"></slot>
       </div>
   </form>
 </template>
+
--- src/views/SearchBarView.vue
+++ src/views/SearchBarView.vue
@@ -2,12 +2,6 @@
 import { ref, computed } from 'vue'
 import SearchBar from '../components/SearchBar.vue'
 const fruits = ref(["apple", "banana", "orange",'kiwi']);
-// let inputEl = ref("");
-// const filterFruit = ()=>{
-//   return fruits.filter((fruit) =>
-//     fruit.toLowerCase().includes(inputEl.value.toLowerCase())
-//   );
-// }
 const newFruit = ref<string[]>([])
 //從子層傳過來的值
 const parentValue = ref('')
@@ -17,20 +11,27 @@ const handleSubmit = ()=>{
   );
   parentValue.value = ''
 }
+
 </script>
 <template>
   <div>
     <h2 class="mb-4">Search Barsddfe</h2>
-    <SearchBar>
-      <template #icon>
-        <div class="absolute inset-y-0 left-0 flex z-10 items-center pl-3 pointer-events-none">
-          <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-        </div>
-      </template>
+    <!-- 使用預設 -->
+    <SearchBar class="mb-4" :default-icon="true">
       <template #button>
         <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
       </template>
     </SearchBar>
+    <SearchBar :focus-color="'focus:ring-red-500'" >
+        <template #icon>
+          <div class="absolute inset-y-0 left-0 flex z-10 items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+          </div>
+        </template>
+        <template #button>
+          <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Search</button>
+        </template>
+      </SearchBar>
     <h3>the other</h3>
     <SearchBar>
         <template #button>
@@ -39,7 +40,7 @@ const handleSubmit = ()=>{
           </button>
         </template>
     </SearchBar>
-    <h3 class="mt-2">Implement Example</h3>
+    <h3 class="mt-2">Implement search Example</h3>
     <!-- //https://blog.logrocket.com/create-search-bar-vue/ -->
     <SearchBar  v-model:input-value="parentValue" @submit.prevent="handleSubmit">
       <template #icon>
@@ -54,10 +55,6 @@ const handleSubmit = ()=>{
     <p class="font-bold">filter fruit List</p>
     <div v-for="fruit in fruits" :key="fruit">
       <p>{{ fruit }}</p>
-    </div>
-    <div class="mt-2">
-      <span class="bg-cyan-500 p-2">after submit</span>
-      {{ newFruit }}
     </div>
   </div>
 </template>
