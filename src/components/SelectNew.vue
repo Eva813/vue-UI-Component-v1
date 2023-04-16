d0db1818e3a6d5e246d8e7b636cfb8de9c266287
Eva <as45986@gmail.com>
2023-04-16 15:18:06 +0800

    feat:add new select

---
+++ src/components/SelectNew.vue
@@ -0,0 +1,171 @@
+<script setup lang="ts">
+import { ref, computed, onMounted } from 'vue'
+import type { PropType } from 'vue'
+
+export interface Option {
+  value: string;
+  label: string;
+}
+export interface Props {
+  options?: Option[],
+  placeholder: string,
+  clearable?: boolean,
+  multiple?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  // options: () => [
+  //   { label: 'Option 1', value: 'option1' },
+  //   { label: 'Option 2', value: 'option2' },
+  //   { label: 'Option 3', value: 'option3' },
+  // ],
+});
+
+
+const selectedOption = ref<{ label: string, value: string } | null >(null)
+const selectedOptions = ref<{ label: string, value: string }[]>([])
+const isDropdownOpen = ref(false)
+const emit = defineEmits(["update:selected-values"]);
+
+const selectedValues = computed({
+  get() {
+    return selectedOptions.value.map(option => option.value);
+  },
+  set(values) {
+    // Note: we are using destructuring assignment syntax here.
+    selectedOptions.value = props.options!.filter((option:{value: string}) => values.includes(option.value));
+    emit('update:selected-values', props.multiple ? selectedOptions.value : selectedOption.value);
+  }
+})
+const toggleDropdown = ()=>{
+  isDropdownOpen.value = !isDropdownOpen.value
+}
+
+onMounted(() => {
+  // console.log(props.options)
+  // if (props.multiple) {
+  //   selectedOptions.value = props.options.filter(option => props.value && props.value.map(v => v.value).includes(option.value));
+  // } else {
+  //   if( selectedOption.value !== undefined){
+  //     selectedOption.value = props.options!.find((option, index) => option.value === props.value);
+  //   }
+
+  // }
+})
+
+const selectOption = (option:any)=>{
+  if (props.multiple) {
+    if (!selectedOptions.value.find(selected => selected.value === option.value)) {
+      selectedOptions.value.push(option);
+    }
+  } else {
+    selectedOption.value = option;
+    isDropdownOpen.value = false;
+    emit('update:selected-values', selectedOption.value);
+  }
+}
+
+const clearSelection = ()=>{
+  if (props.multiple) {
+    selectedOptions.value = [];
+    emit('update:selected-values', []);
+  } else {
+    selectedOption.value = null;
+    emit('update:selected-values', null);
+  }
+}
+
+
+</script>
+<template>
+  <div class="select-container">
+    <div class="select-input" @click="toggleDropdown">
+      <span v-if="selectedOptions.length > 0">{{ selectedOptions.map(option => option.label).join(', ') }}</span>
+      <span v-else-if="selectedOption?.hasOwnProperty('label')">{{ selectedOption?.label || ''}}</span>
+      <span v-else>{{ placeholder }}</span>
+      <!-- icon的引入! -->
+      <div class="icons ml-auto flex items-center justify-center">
+        <button v-if="clearable && (selectedOption || selectedOptions.length > 0)" @click.native.stop="clearSelection"><span class="material-icons-sharp block">cancel</span></button>
+        <button ><span class="material-icons-outlined arrow-icon  block">expand_more</span></button>
+
+      </div>
+    </div>
+    <ul v-if="isDropdownOpen" class="select-dropdown z-10">
+      <li v-if="multiple" class="select-option" v-for="option in options" :key="option.value">
+        <label>
+          <input type="checkbox" :value="option.value" v-model="selectedValues">
+          {{ option.label }}
+        </label>
+      </li>
+      <li v-else class="select-option" v-for="option in options" :key="option.value" @click="selectOption(option)">
+        {{ option.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+<style scoped>
+.select-container {
+  position: relative;
+}
+
+.select-input {
+  cursor: pointer;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+}
+
+.arrow-icon {
+  margin-left: 8px;
+  transition: transform 0.2s ease-in-out;
+}
+
+.arrow-up {
+  transform: rotate(180deg);
+}
+
+.arrow-down {
+  transform: rotate(0deg);
+}
+
+.clear-icon {
+  margin-left: auto;
+  cursor: pointer;
+}
+
+.select-dropdown {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  max-height: 200px;
+  overflow-y: auto;
+  list-style: none;
+  padding: 0;
+  margin-top: 4px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.select-option {
+  cursor: pointer;
+  padding: 8px;
+  transition: background-color 0.2s ease-in-out;
+}
+
+.select-option:hover {
+  background-color: #f2f2f2;
+}
+
+.select-option label {
+  display: flex;
+  align-items: center;
+}
+
+.select-option input[type="checkbox"] {
+  margin-right: 8px;
+}
+</style>
---
+++ src/views/SelectView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import Select from '../components/Select.vue'
+import SelectNew from '../components/SelectNew.vue'
+const selectedValue1 = ref('')
+const selectedValue2 = ref([])
+const optionsOne = ref([
+  { label: 'Apple', value: 'Apple' },
+  { label: 'Kiwi', value: 'Kiwi' },
+  { label: 'Guava', value: 'Guava' },
+])
+
+const optionsThree = ref([
+  { label: 'One', value: 'One' },
+  { label: 'Two', value: 'Two' },
+  { label: 'Three', value: 'Three' },
+])
+const selectedValue3 = ref({})
+const optionsFour = ref([
+  { label: 'One 1', value: 'One' },
+  { label: 'Two 2', value: 'Two' },
+  { label: 'Three 3', value: 'Three' },
+])
+const selectedValue4 = ref([])
+</script>
+<template>
+  <div>
+    <h1>My select demo</h1>
+    <Select v-model:selected-values="selectedValue1" :options="optionsOne" ></Select>
+    {{ selectedValue1 }}
+    <h2>multiple select</h2>
+    <Select v-model:selected-values="selectedValue2" :multiple="true" ></Select>
+    {{ selectedValue2 }}
+    <h2> 測試新 select 單選 </h2>
+    <div class="p-3 mb-2 bg-gray-200 rounded">
+      <SelectNew :options="optionsThree"  v-model:selected-values="selectedValue3" :placeholder="'請選擇..'" :clearable="false" ></SelectNew>
+      <div>選到的 {{ selectedValue3 }}</div>
+    </div>
+    <div class="p-3 bg-gray-200 rounded">
+      <h2> 測試新 select 多選 </h2>
+      <SelectNew :options="optionsFour" v-model:selected-values="selectedValue4" :placeholder="'請選擇多個..'" :clearable="true" :multiple="true"></SelectNew>
+      <div>選到的 {{ selectedValue4 }}</div>
+    </div>
+  </div>
+</template>
