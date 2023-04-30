07bc0ffaf22b8417c311926b3f8fc2095ddab8f4
Eva <as45986@gmail.com>
2023-04-30 09:07:41 +0800

    fix:modify checkbox component

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,99 +1,65 @@
-<!-- <template>
-  <div>
-    <div class="form-check-label">
-      <label class="inline-flex " >
-        checkbox
-        <input  type="checkbox" class='form-check-input absolute opacity-0 cursor-pointer' />
-        <div class="jx-check-bg flex items-center justify-center w-[1.6rem] h-[1.6rem] border-[#ccc] mt-[0.4rem] border-[0.2rem] rounded-sm checked:bg-[#ccc] cursor-pointer bg-contain bg-no-repeat">
-          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10" class="w-full h-full checked:opacity-1">
-            <polyline class="jx-check-path" fill="none" stroke="#FFF" stroke-linecap="square" stroke-width="2" points="3.4 8.9 6.2 11.8 12.6 5.4" transform="translate(-2 -3)" />
-            </svg>
-        </div>
-      </label>
-    </div>
-  </div>
-</template> -->
 <script setup lang="ts">
-import { ref } from 'vue'
-const isChecked = ref(false)
-const  toggleCheckbox = ()=>{
-  console.log('click')
-  isChecked.value = !isChecked.value
+import { ref, computed } from 'vue'
+// 外層傳給內的值 label? and value
+//Indeterminate
+//參考：https://dev.to/maxwelladapoe/creating-a-custom-checkbox-component-in-vue-3-a-better-approach-2jjd
+//當 disable 的時候顏色調整
+export interface Props {
+  // disabled: boolean,
+  modelValue: object[] | boolean,
+  value?: object | boolean,
+  label?: string,
+  indeterminate?: boolean,
+  disabled: boolean
 }
+
+const props = withDefaults(defineProps<Props>(), {
+  indeterminate: false
+});
+
+const isChecked = ref(false)
+const emit = defineEmits(["update:modelValue"]);
+const checkedValue = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    console.log(value)
+    isChecked.value = !isChecked.value;
+    emit("update:modelValue", value);
+  },
+});
+
 </script>
 <template>
   <div class="">
     <div class="flex items-start">
       <label class="flex items-center text-sm text-gray-700">
-        <input type="checkbox" class="hidden form-checkbox" @click="toggleCheckbox"  v-model="isChecked" />
-        <div class="relative flex items-center justify-center  mr-2 rounded border-[0.2rem] border-gray-400 transition-colors w-[1.6rem] h-[1.6rem]" :class="{'bg-indigo-500': isChecked}">
+        <input type="checkbox" class="hidden form-checkbox"  :value="value" v-model="checkedValue" :disabled="disabled" />
+        <div class="relative flex items-center justify-center  mr-2 rounded border-[0.2rem] border-gray-400 transition-colors w-[1.6rem] h-[1.6rem] hover:shadow-md" :class="{'bg-gray-400': isChecked , 'is-indeterminate': indeterminate }">
             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10" class="w-full h-full" v-if="isChecked">
               <polyline class="jx-check-path" fill="none" stroke="#fff" stroke-linecap="square" stroke-width="2"
                   points="3.4 8.9 6.2 11.8 12.6 5.4" transform="translate(-2 -3)" />
-                  </svg>
+                  <!-- 替勾選加入動畫 -->
+                  <animate attributeName="stroke-dashoffset" dur="300ms" begin="0s" from="20" to="0" fill="freeze" />
+              </svg>
         </div>
-        checkbox {{ isChecked }}
+        {{ label }}
+        <slot name="labelContext"></slot>
       </label>
     </div>
   </div>
 
 </template>
 
-<style >
-/* .form-checkbox:checked {
-  @apply bg-gray-400 border-gray-400;
-}
-.form-checkbox:focus {
-  @apply outline-none ring-2 ring-offset-2 ring-offset-gray-100 ring-indigo-500;
-}
-.form-checkbox ~ .relative {
-  @apply border-l-0 border-t-0 border-r-0 border-b-2;
-}
-.form-checkbox:checked ~ .relative svg {
-  @apply opacity-100;
-} */
-/* .form-check-label {
-  display: inline-flex;
-  align-items: flex-start;
-  font-size: 16px;
-  color: #000;
+<style>
 
-} */
-
-/* .form-check-input {
-
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-} */
-
-/* .jx-check-bg {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: .4rem;
-  background-color: #fff;
-  border: .2rem solid #ccc;
-  transition: .2s;
- width: 1.6rem;
-      height: 1.6rem;
-      border-radius: .2rem;
-      background-size: contain;
-      background-repeat: no-repeat;
- 
-} */
- /* svg {
-        width: 100%;
-        height: 100%;
- } */
 /* 打勾動畫 */
 .jx-check-path {
   stroke-dasharray: 20;
 }
 
- .form-check-label:hover .form-check-input:not(:disabled) {
-    border-color: yellow;
-  }
+
 @keyframes checked {
   from {
     stroke-dashoffset: 20;
@@ -103,4 +69,12 @@ const  toggleCheckbox = ()=>{
     stroke-dashoffset: 0;
   }
 }
+
+.is-indeterminate {
+  background-color: #ccc;
+  background-image: url('../assets/icon-checked-partial.svg');
+  background-repeat: no-repeat;
+  background-position: center;
+  color: aliceblue;
+  }
 </style>
--- src/views/CheckboxView.vue
+++ src/views/CheckboxView.vue
@@ -1,11 +1,53 @@
 <script setup lang="ts">
 import Checkbox from '../components/Checkbox.vue'
+import {ref} from "vue"
+
+
+const selectedPeople = ref([]);
+const isSelected = ref(false);
+const isSelectedAgree = ref(false);
+const people = ref([
+  {
+    name: "William",
+    age: 30,
+    sex: "Male"
+  },
+  {
+    name: "Thomas",
+    age: 24,
+    sex: "Male"
+  },
+  {
+    name: "Jennifer",
+    age: 20,
+    sex: "Female"
+  }
+]);
+
 
 </script>
 
 <template>
   <div>
-    <h2 class="text-xl">checkbox view</h2>
-    <Checkbox>ewd</Checkbox>
+    <h2 class="text-xl">單一個 checkbox</h2>
+    <Checkbox :disabled="false" :label="'單一'" v-model:model-value="isSelected" ></Checkbox>
+    <div>
+      <h2>有多個 checkbox 並透過勾選存於同一個陣列中</h2>
+      <p>勾選到的資料：{{ selectedPeople }}</p>
+      <template v-for="person in people" :key="person.name" >
+        <Checkbox  :disabled="false" :label="person.name" v-model:model-value="selectedPeople" :value="person"
+        ></Checkbox>
+      </template>
+    </div>
+    <div>
+      <h2>label 插入更多資訊</h2>
+      <Checkbox :disabled="false" v-model:model-value="isSelectedAgree">
+        <template v-slot:labelContext>
+          <div>
+            I agree that <a>agreement doc</a>
+          </div>
+        </template>
+      </Checkbox>
+    </div>
   </div>
 </template>
