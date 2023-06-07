7b7249172d7d5218d03e1ddc220e2f73ee8a94b7
Eva <as45986@gmail.com>
2023-06-07 21:56:53 +0800

    modify page

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,25 +1,3 @@
-<!-- <template>
-  <div>
-    <h1>test add input</h1>
-    <div>
-      <input class="border" type="text" v-for="(item, i) of items" v-model="items[i]" :key="i">
-      <button @click="onAdd">添加</button>
-      <div>{{ items }}</div>
-    </div>
-  </div>
-</template>
-
-<script lang="ts">
-export default {
-  data() {
-    return {
-      items: []
-    }
-  },
-  methods: {
-    onAdd() {
-      this.items.push('')
-    }
-  }
-}
-</script> -->
+ <template>
+  
+ </template>
